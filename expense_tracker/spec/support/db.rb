d3a1f70e36b072196f946d3d5f9cfa2936f539f8
m-gb <mayu666@gmail.com>
2017-10-13 10:01:37 +0200

    added metadata

--- expense_tracker/spec/support/db.rb
+++ expense_tracker/spec/support/db.rb
@@ -4,11 +4,17 @@ RSpec.configure do |c|
     Sequel.extension :migration
     Sequel::Migrator.run(DB, 'db/migrations')
     DB[:expenses].truncate # Removes any leftover test data from the table.
+
+    FileUtils.mkdir_p('log') # Configures sequel to record each executed sql statement to the log file.
+    require 'logger'
+    DB.loggers << Logger.new('log/sequel.log')
   end
 
   # Avoids littering each db-dependent spec.
   # Loads for any spec that touches the db, but not for unit specs(by requiring it).
   c.around(:example, :db) do |example|
+    DB.log_info "Starting example: #{example.metadata[:description]}"
     DB.transaction(rollback: :always) { example.run }
+    DB.log_info "Ending example: #{example.metadata[:description]}"
   end
 end
--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -1,5 +1,4 @@
 # require 'pp'
-
 # RSpec.describe Hash, :outer_group do
 #   it 'is used by RSpec for metadata', :fast, :focus do |example|
 #     pp example.metadata # Returns a hash  containing all the metadata.
@@ -22,19 +21,42 @@
 #     meta[:fast] = true
 #   end
 # end
-#
+
 # Testing Rails models:
 # RSpec.configure do |config|
 #   config.define_derived_metadata(type: :model) do |meta|
 #     # ...
 #   end
 # end
-#
+
 # Specs will report every failed expectation:
 # RSpec.configure do |config|
 #   config.define_derived_metadata do |meta|
-#     # Sets the flag unconditionally;
-#     # doesn't allow examples to opt out
-#     meta[:aggregate_failures] = true
+#     meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures) # Checks if the key exists before overriding it.
 #   end
 # end
+
+# Lets individual examples opt out of the behavior:
+# RSpec.describe 'Billing', aggregate_failures: false do
+#   context 'using the fake payment service' do
+#     before do
+#       expect(MyApp.config.payment_gateway).to include('sandbox')
+#     end 
+#     #...
+#   end
+# end
+
+# Excluding examples
+# /spec/spec_helper.rb
+RSpec.configure do |config|
+  config.filter_run_excluding :jruby_only unless RUBY_PLATFORM == 'java'
+end
+
+# Including examples
+# /spec/spec_helper.rb
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus # If nothing matches the filter, RSpec ignores it.
+end
+
+# $rspec --tag fast # Runs just the examples tagged with :fast.
+# $rspec --tag ~fast # Runs all examples that lack the :fast tag.
